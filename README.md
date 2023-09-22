a3a0e3686883f8b3a03f00b9e3292e159c7fc1a2
Sambo Chhay <sambo@pipay.com>
2023-09-22 16:11:48 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Internet Bakong
+
+We are going to develop internet banking API concept with spring boot based microserices architecture.
---
+++ api-gateway/src/test/java/com/sambochhay/finance/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sambochhay.finance.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
